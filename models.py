5af2b6be097eaae5c317b721352a97656a47298e
Никита Исаев <0x142>
2021-07-31 15:48:46 +0300

    Add new model

--- models.py
+++ models.py
@@ -11,20 +11,23 @@ np.random.seed(42)
 class CNNClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 8, kernel_size=3)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3)
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=3)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
         self.max_pool = nn.MaxPool2d(2)
         self.flatten = nn.Flatten()
-        self.linear_1 = nn.Linear(32, 16)
-        self.linear_2 = nn.Linear(16, 1)
+        self.linear_1 = nn.Linear(64*2*2, 512)
+        self.linear_2 = nn.Linear(512, 1)
         
     def conv(self, x):
-        x = self.conv1(x)
+        x = self.conv1(x) 
+        x = F.leaky_relu(x)
         x = self.max_pool(x)
         x = self.conv2(x)
-        x = self.max_pool(x)
+        x = F.leaky_relu(x)
         x = self.conv3(x)
+        x = F.leaky_relu(x)
+        x = self.max_pool(x)
         x = self.flatten(x)
         
         return x
@@ -32,7 +35,7 @@ class CNNClassifier(nn.Module):
     def ffnn(self, x):
         x = self.linear_1(x)
         x = torch.relu(x)
-        x = F.dropout(x, p=0.2)
+      #  x = F.dropout(x, p=0.2)
         x = self.linear_2(x)
         x = torch.sigmoid(x)
         
